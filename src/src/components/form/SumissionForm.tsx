08c611d2b90470f6c5687a2502a0a4bd58e4e64f
dinhbinhnguyen0105 <dinhbinhnguyen0105@gmail.com>
2025-01-14 19:23:13 +0700

    'form' from macmini

--- src/src/components/form/SumissionForm.tsx
+++ src/src/components/form/SumissionForm.tsx
@@ -6,7 +6,67 @@ const SubmissionForm: React.FC = () => {
 
     return (
         <div className={styles.submissionForm}>
-            <div className="intro"></div>
+            <div className={styles.intro}>
+                <ul>
+                    <li>
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="rgb(186,155,231)"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <g id="Page-1" stroke="none" strokeWidth="1" fill="rgb(186,155,231)" fillRule="evenodd">
+                                <g id="Icon-Set" transform="translate(-100 -1139)" fill="rgb(255,255,255)">
+                                    <path d="M122.027 1148.07C121.548 1147.79 120.937 1147.96 120.661 1148.43L114.266 1159.51L110.688 1156.21C110.31 1155.81 109.677 1155.79 109.274 1156.17C108.871 1156.54 108.85 1157.18 109.228 1157.58L113.8 1161.8C114.177 1162.2 114.81 1162.22 115.213 1161.84C115.335 1161.73 122.393 1149.43 122.393 1149.43C122.669 1148.96 122.505 1148.34 122.027 1148.07Z M116 1169C108.268 1169 102 1162.73 102 1155C102 1147.27 108.268 1141 116 1141C123.732 1141 130 1147.27 130 1155C130 1162.73 123.732 1169 116 1169Z M116 1139C107.164 1139 100 1146.16 100 1155C100 1163.84 107.164 1171 116 1171C124.836 1171 132 1163.84 132 1155C132 1146.16 124.836 1139 116 1139Z" id="checkmark-circle" />
+                                </g>
+                            </g>
+                        </svg>
+                        <h3>Quick and free sign-up</h3>
+                        <p>Enter your email address to create an account.</p>
+                    </li>
+                    <li>
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="rgb(186,155,231)"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <g id="Page-1" stroke="none" strokeWidth="1" fill="rgb(186,155,231)" fillRule="evenodd">
+                                <g id="Icon-Set" transform="translate(-100 -1139)" fill="rgb(255,255,255)">
+                                    <path d="M122.027 1148.07C121.548 1147.79 120.937 1147.96 120.661 1148.43L114.266 1159.51L110.688 1156.21C110.31 1155.81 109.677 1155.79 109.274 1156.17C108.871 1156.54 108.85 1157.18 109.228 1157.58L113.8 1161.8C114.177 1162.2 114.81 1162.22 115.213 1161.84C115.335 1161.73 122.393 1149.43 122.393 1149.43C122.669 1148.96 122.505 1148.34 122.027 1148.07Z M116 1169C108.268 1169 102 1162.73 102 1155C102 1147.27 108.268 1141 116 1141C123.732 1141 130 1147.27 130 1155C130 1162.73 123.732 1169 116 1169Z M116 1139C107.164 1139 100 1146.16 100 1155C100 1163.84 107.164 1171 116 1171C124.836 1171 132 1163.84 132 1155C132 1146.16 124.836 1139 116 1139Z" id="checkmark-circle" />
+                                </g>
+                            </g>
+                        </svg>
+                        <h3>Cross-platform solution</h3>
+                        <p>Preview your newsletters on any device befor sending them out.</p>
+                    </li>
+                    <li>
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="rgb(186,155,231)"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <g id="Page-1" stroke="none" strokeWidth="1" fill="rgb(186,155,231)" fillRule="evenodd">
+                                <g id="Icon-Set" transform="translate(-100 -1139)" fill="rgb(255,255,255)">
+                                    <path d="M122.027 1148.07C121.548 1147.79 120.937 1147.96 120.661 1148.43L114.266 1159.51L110.688 1156.21C110.31 1155.81 109.677 1155.79 109.274 1156.17C108.871 1156.54 108.85 1157.18 109.228 1157.58L113.8 1161.8C114.177 1162.2 114.81 1162.22 115.213 1161.84C115.335 1161.73 122.393 1149.43 122.393 1149.43C122.669 1148.96 122.505 1148.34 122.027 1148.07Z M116 1169C108.268 1169 102 1162.73 102 1155C102 1147.27 108.268 1141 116 1141C123.732 1141 130 1147.27 130 1155C130 1162.73 123.732 1169 116 1169Z M116 1139C107.164 1139 100 1146.16 100 1155C100 1163.84 107.164 1171 116 1171C124.836 1171 132 1163.84 132 1155C132 1146.16 124.836 1139 116 1139Z" id="checkmark-circle" />
+                                </g>
+                            </g>
+                        </svg>
+                        <h3>Start sending emails</h3>
+                        <p>Use our API or pick our pre-built templates.</p>
+                    </li>
+                </ul>
+            </div>
+            <div className={styles.signinForm}>
+                <h2>Create your account</h2>
+                <form action="POST">
+                    <label htmlFor="email-address" className={styles.signinItem}>Email
+                        <input className={styles.signinItemInput} type="text" id="email-address" placeholder="Enter your email address" />
+                    </label>
+                    <label htmlFor="fullname" className={styles.signinItem}>Full name
+                        <input className={styles.signinItemInput} type="text" id="fullname" placeholder="Enter your full name" />
+                    </label>
+                    <label htmlFor="password" className={styles.signinItem}>Password
+                        <input className={styles.signinItemInput} type="text" id="password" placeholder="Type to create a password" />
+                    </label>
+                    <div className={`${styles.signinItem} ${styles.getNotif}`}>
+                        <label htmlFor="notification" className={styles.signinItem}>
+                            <input className={styles.signinItemInput} type="checkbox" id="notification" />
+                            Get updates and notifications about our product.
+                        </label>
+                    </div>
+                    <button type="submit" className={styles.submitBtn}>Type to create a password</button>
+                </form>
+            </div>
         </div>
     );
 }
